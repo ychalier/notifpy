3b155b805f74ca7e8995756c870247cb4dcbdebf
ychalier <yohan.chalier@telecom-paristech.fr>
2018-03-01 13:46:41 +0100

    username handling

--- notif.py
+++ notif.py
@@ -1,9 +1,11 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import urllib.request
+import datetime
 import sqlite3
 import smtplib
 import json
+import time
 import re
 
 
@@ -37,6 +39,18 @@ class API:
             self.conn.commit()
         except sqlite3.OperationalError:
             pass
+        try:
+            c.execute('CREATE TABLE channels (id TEXT, pattern TEXT)')
+            self.conn.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def retrieve_channel_id(self, username):
+        href = "https://www.googleapis.com/youtube/v3/channels?part=snippet" \
+               + "&forUsername=" + username\
+               + "&key=" + self.settings["google_api_key"]
+        response = urllib.request.urlopen(href)
+        return json.loads(response.read().decode('utf-8'))['items'][0]['id']
 
     def retrieve_video_list(self, channel_id):
         href = "https://www.googleapis.com/youtube/v3/search?order=date" \
@@ -57,6 +71,7 @@ class API:
                      vid['snippet']['thumbnails']['medium']['url'])\
             .replace("%VIDEO_TITLE%", vid['snippet']['title'])
         self.send_mail(subject, body)
+        log("Sending mail " + subject + " to " + self.settings["smtp_address"])
 
     def send_mail(self, subject, body):
         msg = MIMEMultipart()
@@ -65,12 +80,14 @@ class API:
         msg['Subject'] = subject
         msg.attach(MIMEText(body, 'html'))
         if self.server is None:
+            log("Connecting to SMTP server")
             self.server = smtplib.SMTP("smtp.gmail.com", 587)
             self.server.ehlo()
             self.server.starttls()
             self.server.ehlo()
             self.server.login(self.settings["smtp_username"],
                               self.settings["smtp_password"])
+            log("Connected to SMTP server")
         self.server.sendmail(self.settings["smtp_address"],
                              self.settings["smtp_address"],
                              msg.as_string())
@@ -85,16 +102,34 @@ class API:
             return True
         return False
 
+    def inspect_channel(self, channel_id, pattern):
+        c = self.conn.cursor()
+        c.execute('INSERT INTO channels VALUES (?, ?)', (channel_id, pattern))
+        self.conn.commit()
+
+    def inspect_username(self, username, pattern):
+        self.inspect_channel(self.retrieve_channel_id(username), pattern)
+
     def check_for_news(self, channel_id, pattern):
         vids = self.retrieve_matching_videos(channel_id, pattern)
         for vid in vids:
             if self.is_new(channel_id, vid['id']['videoId']):
+                log("New video from " + channel_id
+                    + ": " + vid['id']['videoId'])
                 self.notify_video(vid)
+
+    def update(self):
+        c = self.conn.cursor()
+        c.execute('SELECT * FROM channels')
+        for channel_id, pattern in c.fetchall():
+            log("Checking channel " + channel_id)
+            self.check_for_news(channel_id, pattern)
 
     def close(self):
         self.conn.close()
         if self.server is not None:
             self.server.close()
+
 
 def apply_mask(videos, pattern):
     prog = re.compile(pattern)
@@ -106,8 +141,13 @@ def apply_mask(videos, pattern):
     return filtered_videos
 
 
+def log(text):
+    timestamp = datetime.datetime.fromtimestamp(
+        time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    print(timestamp + "\t" + text)
+
+
 if __name__ == "__main__":
     api = API()
-    api.check_for_news("UCOLBuf1DXEfdIccvGczNUzg", "Yu-Gi-Oh!")
+    api.update()
     api.close()
-
