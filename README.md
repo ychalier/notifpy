a4f7be1bf6b497acacbbc5d86f691297beaed3dd
ychalier <yohan.chalier@telecom-paristech.fr>
2018-03-01 11:49:59 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # notif.py
+
+## install
+
+    git clone https://github.com/ychalier/notif.py.git
+    cd notif.py/
+
+## .credentials
+
+Retrieve a Google API key from http://console.developers.google.com
---
+++ notif.py
@@ -0,0 +1,90 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import urllib.request
+import smtplib
+import json
+import re
+
+
+class API:
+
+    def __init__(self):
+        self.settings = {}
+        self.server = None
+        self.read_settings()
+
+    def read_settings(self):
+        settings = {}
+        file_object = open('.credentials', 'r')
+        for line in file_object.readlines():
+            line = line.replace('\n', '')
+            settings[line.split('=')[0]] = line.split('=')[1]
+        for ts in ["google_api_key", "smtp_host", "smtp_username",
+                   "smtp_password", "smtp_address"]:
+            if ts not in settings.keys():
+                print("[ERROR] Setting not found: " + ts)
+                exit()
+        file_object.close()
+        self.settings = settings
+
+    def retrieve_video_list(self, channel_id):
+        href = "https://www.googleapis.com/youtube/v3/search?order=date" \
+               + "&part=snippet&channelId=" + channel_id \
+               + "&maxResults=25&key=" + self.settings["google_api_key"]
+        response = urllib.request.urlopen(href)
+        return json.loads(response.read().decode('utf-8'))['items']
+
+    def retrieve_matching_videos(self, channel_id, pattern):
+        return apply_mask(self.retrieve_video_list(channel_id), pattern)
+
+    def notify_video(self, vid):
+        subject = "[notif.py] New video from " + vid['snippet']['channelTitle']
+        body = open('mail-template.html', 'r').read()\
+            .replace("%CHANNEL_TITLE%", vid['snippet']['channelTitle'])\
+            .replace("%VIDEO_ID%", vid['id']['videoId'])\
+            .replace("%THUMBNAIL_URL%",
+                     vid['snippet']['thumbnails']['medium']['url'])\
+            .replace("%VIDEO_TITLE%", vid['snippet']['title'])
+        self.send_mail(subject, body)
+
+    def send_mail(self, subject, body):
+        msg = MIMEMultipart()
+        msg['From'] = self.settings["smtp_address"]
+        msg['To'] = self.settings["smtp_address"]
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'html'))
+        if self.server is None:
+            self.server = smtplib.SMTP("smtp.gmail.com", 587)
+            self.server.ehlo()
+            self.server.starttls()
+            self.server.ehlo()
+            self.server.login(self.settings["smtp_username"],
+                              self.settings["smtp_password"])
+        self.server.sendmail(self.settings["smtp_address"],
+                             self.settings["smtp_address"],
+                             msg.as_string())
+
+    def is_new(self, channel_id, video_id):
+        return True
+
+    def check_for_news(self, channel_id, pattern):
+        vids = self.retrieve_matching_videos(channel_id, pattern)
+        for vid in vids:
+            if self.is_new(channel_id, vid['id']['videoId']):
+                self.notify_video(vid)
+
+
+def apply_mask(videos, pattern):
+    prog = re.compile(pattern)
+    filtered_videos = []
+    for vid in videos:
+        result = prog.match(vid['snippet']['title'])
+        if result is not None:
+            filtered_videos.append(vid)
+    return filtered_videos
+
+
+if __name__ == "__main__":
+    api = API()
+    api.check_for_news("UCOLBuf1DXEfdIccvGczNUzg", "Yu-Gi-Oh!")
+    api.server.close()
