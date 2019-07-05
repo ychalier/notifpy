d6e1d7c4ba9fab8a98c58eca8f6236a60576022c
ychalier <yohan.chalier@telecom-paristech.fr>
2019-07-05 09:31:14 +0200

    Removed old files

--- notifpy/__init__.py
+++
@@ -1,4 +0,0 @@
-import notifpy.templates
-import notifpy.db
-import notifpy.yt
-from .manager import Manager
--- notifpy/db/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import Database
-from .channel import Channel
-from .video import Video
-from .pattern import Pattern
