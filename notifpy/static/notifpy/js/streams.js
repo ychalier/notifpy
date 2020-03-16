372048edede35b4f8b681b14f3cd177e295f18d9
ychalier <yohan.chalier@telecom-paristech.fr>
2020-03-16 11:59:30 +0100

    Added stream thumbnail fade in

--- notifpy/static/notifpy/js/streams.js
+++ notifpy/static/notifpy/js/streams.js
@@ -4,23 +4,35 @@ console.log("Stream route: " + STREAMS_ROUTE);
 let request = new XMLHttpRequest();
 request.open("GET", STREAMS_ROUTE, true);
 
+var template = document.querySelector("#template--stream");
+var streamList = document.querySelector("#stream_list");
+streamList.innerHTML = "";
+var loaded = 0;
+
+function appendStream(data, i) {
+    let clone = document.importNode(template.content, true);
+    let icon = clone.querySelector(".channel__thumbnail");
+    icon.alt = data[i].name;
+    icon.onload = function () {
+        loaded++;
+        if (loaded === data.length) {
+            streamList.classList.add("channel-banner--expanded");
+        }
+    }
+    clone.querySelector(".tooltip__user").textContent = data[i].name;
+    clone.querySelector(".tooltip__game").textContent = data[i].game;
+    clone.querySelector(".tooltip__title").textContent = data[i].title;
+    clone.querySelector(".tooltip__screen").src = data[i].screen;
+    icon.src = data[i].thumb;
+    streamList.appendChild(clone);
+}
+
 request.onload = function() {
     if (request.readyState === 4 && request.status == 200) {
         let data = JSON.parse(request.response);
         console.log("Found " + data.length + " stream(s)");
-        let template = document.querySelector("#template--stream");
-        let streamList = document.querySelector("#stream_list");
-        streamList.innerHTML = "";
         for (let i = 0; i < data.length; i++) {
-            let clone = document.importNode(template.content, true);
-            clone.querySelector(".image-link").href = data[i].lnk;
-            clone.querySelector(".channel__thumbnail").src = data[i].thumb;
-            clone.querySelector(".channel__thumbnail").alt = data[i].name;
-            clone.querySelector(".tooltip__user").textContent = data[i].name;
-            clone.querySelector(".tooltip__game").textContent = data[i].game;
-            clone.querySelector(".tooltip__title").textContent = data[i].title;
-            clone.querySelector(".tooltip__screen").src = data[i].screen;
-            streamList.appendChild(clone);
+            appendStream(data, i);
         }
     }
 }
